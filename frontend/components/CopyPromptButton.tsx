34a0f2bde033ad943d5c9fe8f16fe1ffd963f1b6
parsehex <1779937+parsehex@users.noreply.github.com>
2023-05-09 17:07:32 -0400

    Update CopyPromptButton.tsx

--- frontend/components/CopyPromptButton.tsx
+++ frontend/components/CopyPromptButton.tsx
@@ -22,7 +22,34 @@ const CopyPromptButton: React.FC<CopyPromptButtonProps> = ({
 		}
 
 		const prompt = makePrompt(promptParts);
-		navigator.clipboard.writeText(prompt);
+		if (!navigator.clipboard) {
+			try {
+				// make a hidden span with the prompt text
+				const hiddenSpan = document.createElement('span');
+				hiddenSpan.textContent = prompt;
+				hiddenSpan.style.display = 'none';
+				document.body.appendChild(hiddenSpan);
+				// highlight the text
+				const range = document.createRange();
+				range.selectNode(hiddenSpan);
+				window.getSelection()?.removeAllRanges();
+				window.getSelection()?.addRange(range);
+				document.execCommand('copy');
+				document.body.removeChild(hiddenSpan);
+				console.log('Fallback: Copying to clipboard was hopefully successful!');
+			} catch (err) {
+				console.error('Fallback: Oops, unable to copy', err);
+			}
+		} else {
+			navigator.clipboard.writeText(prompt).then(
+				function () {
+					console.log('Async: Copying to clipboard was successful!');
+				},
+				function (err) {
+					console.error('Async: Could not copy text: ', err);
+				}
+			);
+		}
 	};
 
 	return <button onClick={copyPromptToClipboard}>Copy Prompt</button>;
