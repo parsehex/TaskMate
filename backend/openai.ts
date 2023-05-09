8874bfeefb3f91478682e8fbab40e2613d2330aa
parsehex <1779937+parsehex@users.noreply.github.com>
2023-05-09 18:43:03 -0400

    update summarize prompt

--- backend/openai.ts
+++ backend/openai.ts
@@ -9,14 +9,14 @@ const chat = new ChatOpenAI({
 });
 
 export const summarize = async (promptPart: any) => {
-	let prompt = `Summarize the following block of text in a succinct but information-rich way.`;
+	let prompt = `Summarize the following block of text in a succinct but information-dense way.`;
 	if (promptPart.part_type === 'file') {
 		const project: any = await db.get(
 			'SELECT name FROM projects WHERE id = ?',
 			[promptPart.project_id]
 		);
 		const projectName = project.name;
-		prompt = `Summarize the following file from the ${projectName} project in a succinct but information-rich way. Include information that other files in the project need to know.`;
+		prompt = `Summarize the following file succinctly but thoroughly. Your summary should detail all exports: functions or methods, variables, types, interfaces, enums, objects, React components, middleware functions, routes, or any other exported entities. Describe their purpose, their parameters (if applicable), their return types (if applicable), and their primary use cases. Include any helper functions or methods and their functionalities. If relevant, mention any classes, interfaces, or data structures that need to be imported from other files to utilize these exports. Focus on providing a practical guide for developers who will use this file, while keeping the summary as compact as possible.`;
 	}
 	const text = promptPart.use_summary ? promptPart.summary : promptPart.content;
 	const response = await chat.call([
