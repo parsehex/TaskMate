82bc32286796d7489dae30fd957084bed3904fea
parsehex <1779937+parsehex@users.noreply.github.com>
2023-05-09 20:46:04 -0400

    restructure scss

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-import './style.scss';
+import './scss/style.scss';
 import { App } from './components/App';
 
 const root = createRoot(document.getElementById('root') as HTMLDivElement);
